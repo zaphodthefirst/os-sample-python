589d1daee5a9366e5423f1b92f40c104278f8994
matthew hudson <matthewhudson@thebigrig.Hudson.home>
2021-03-01 10:38:03 -0500

    change greeting

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "Hello World!"
+    return "Go Fuck yourself Sandiego!"
 
 if __name__ == "__main__":
     application.run()
